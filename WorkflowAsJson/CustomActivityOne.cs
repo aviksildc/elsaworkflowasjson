5fc7750d93d52572011bfecf368a9e6a68c8262c
Avik Sil <avik.sil@datacore.com>
2021-03-29 17:20:31 +0530

    Add solution

---
+++ WorkflowAsJson/CustomActivityOne.cs
@@ -0,0 +1,39 @@
+﻿using Elsa.Services;
+using Elsa.Results;
+using Elsa.Services.Models;
+using System;
+
+namespace Elsa.Test.Activities
+{
+    public class CustomActivityOne : Activity
+    {
+        private readonly IWorkflowExpressionEvaluator expressionEvaluator;
+
+        public CustomActivityOne(IWorkflowExpressionEvaluator expressionEvaluator)
+        {
+            this.expressionEvaluator = expressionEvaluator;
+        }
+
+        public string Input1
+        {
+            get => GetState<string>();
+            set => SetState(value);
+        }
+
+        public string Input2
+        {
+            get => GetState<string>();
+            set => SetState(value);
+        }
+
+        protected override ActivityExecutionResult OnExecute(WorkflowExecutionContext context)
+        {
+            string output = Input1 + ", " + Input2;
+
+            //Console.WriteLine(output);
+            Output.SetVariable("CvdOutput", output);
+            return Done();
+        }
+    }
+
+}
---
+++ WorkflowAsJson/Program.cs
@@ -0,0 +1,44 @@
+﻿using Elsa.Activities.Console.Extensions;
+using Elsa.Models;
+using Elsa.Serialization;
+using Elsa.Serialization.Formatters;
+using Elsa.Services;
+using Elsa.Test.Activities;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Elsa.Test
+{
+    class Program
+    {
+        private static async Task Main()
+        {
+            var json = await ReadEmbeddedResourceAsync("elsaworkflowasjson.workflow.json");
+            var services = BuildServices();
+            var serializer = services.GetRequiredService<IWorkflowSerializer>();
+            var workflow = serializer.Deserialize<WorkflowDefinitionVersion>(json, JsonTokenFormatter.FormatName);
+            var invoker = services.GetRequiredService<IWorkflowInvoker>();
+
+            await invoker.StartAsync(workflow);
+        }
+
+        private static IServiceProvider BuildServices()
+        {
+            return new ServiceCollection()
+                .AddElsa()
+                .AddConsoleActivities()
+                .AddActivity<CustomActivityOne>()
+                .BuildServiceProvider();
+        }
+
+        private static async Task<string> ReadEmbeddedResourceAsync(string resourceName)
+        {
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+            using var reader = new StreamReader(assembly.GetManifestResourceStream(resourceName));
+            return await reader.ReadToEndAsync();
+        }
+    }
+}
